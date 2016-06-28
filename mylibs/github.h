7ddcf36c6095ac52c03dc4b1e4bb7e18a5d3578e
ThEcHaCaL <edluisecosta@gmail.com>
2016-06-27 23:58:53 -0400

    Commited by github.h

--- mylibs/github.h
+++ mylibs/github.h
@@ -1,7 +1,9 @@
 #include <stdlib.h>
 
 void push() {
+
   system("git add /home/edluise/.atom/srcs/cplusplus/*.cpp /home/edluise/.atom/srcs/cplusplus/mylibs/*.h");
   system("git commit -m 'Commited by github.h'");
   system("git push origin master");
+  
 }
