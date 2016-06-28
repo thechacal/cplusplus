45b79f51eccb10e7ba2a74970b2a99c533fbc788
ThEcHaCaL <edluisecosta@gmail.com>
2016-06-27 23:37:56 -0400

    Commited by github.h

---
+++ Exercises from Books/hello.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "../mylibs/github.h"
+using namespace std;
+int main(){
+  cout << "Hello World" << endl;
+
+  push();
+  return 0;
+}
---
+++ mylibs/github.h
@@ -0,0 +1,7 @@
+#include <stdlib.h>
+
+void push() {
+  system("git add /home/edluise/.atom/srcs/cplusplus/*.cpp /home/edluise/.atom/srcs/cplusplus/mylibs/*.h");
+  system("git commit -m 'Commited by github.h'");
+  system("git push origin master");
+}
